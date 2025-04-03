a4542b4bc24bc48cfc7797abf2552055c16b06e5
rylenko <rylenko@tuta.io>
2025-04-03 17:38:52 +0700

    Rewrite tests for root chain

--- rootchain/chain_test.go
+++ rootchain/chain_test.go
@@ -3,7 +3,6 @@ package rootchain
 import (
 	"errors"
 	"reflect"
-	"slices"
 	"testing"
 
 	"github.com/platform-inf/go-ratchet/errlist"
@@ -14,37 +13,54 @@ func TestNewChain(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		rootKey   keys.Root
-		options   []Option
-		errString string
+		name          string
+		rootKey       keys.Root
+		options       []Option
+		errCategories []error
+		errString     string
 	}{
 		{
+			"zero root key and no options",
 			keys.Root{},
+			nil,
 			nil,
 			"",
 		},
 		{
+			"full root key and crypto option",
 			keys.Root{Bytes: []byte{1, 2, 3}},
 			[]Option{WithCrypto(testCrypto{})},
+			nil,
 			"",
 		},
 		{
+			"zero key and crypto option error",
 			keys.Root{},
 			[]Option{WithCrypto(nil)},
-			"config: option: crypto is nil",
+			[]error{errlist.ErrInvalidValue, errlist.ErrOption},
+			"new config: option: invalid value: crypto is nil",
 		},
 	}
 
 	for _, test := range tests {
-		chain, err := New(test.rootKey, test.options...)
-		if (err == nil && test.errString != "") ||
-			(err != nil && err.Error() != test.errString && !errors.Is(err, errlist.ErrOption)) {
-			t.Fatalf("New(%+v, %+v): expected no error but got %v", test.rootKey, test.options, err)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
 
-		if !reflect.DeepEqual(chain.rootKey, test.rootKey) {
-			t.Fatalf("New(%+v, %+v): invalid root key: %v != %v", test.rootKey, test.options, chain.rootKey, test.rootKey)
-		}
+			chain, err := New(test.rootKey, test.options...)
+			if (err == nil && test.errString != "") || (err != nil && err.Error() != test.errString) {
+				t.Fatalf("New(%+v, %+v): expected error %q but got %+v", test.rootKey, test.options, test.errString, err)
+			}
+
+			for _, errCategory := range test.errCategories {
+				if !errors.Is(err, errCategory) {
+					t.Fatalf("New(%+v, %+v): expected error category %v but got %v", test.rootKey, test.options, errCategory, err)
+				}
+			}
+
+			if !reflect.DeepEqual(chain.rootKey, test.rootKey) {
+				t.Fatalf("New(%+v, %+v): invalid root key: %v != %v", test.rootKey, test.options, chain.rootKey, test.rootKey)
+			}
+		})
 	}
 }
 
@@ -57,44 +73,61 @@ func TestChainAdvance(t *testing.T) {
 	}
 
 	tests := []struct {
+		name      string
 		sharedKey keys.Shared
 	}{
-		{},
-		{keys.Shared{Bytes: nil}},
-		{keys.Shared{Bytes: []byte{1, 2, 3, 4, 5}}},
+		{"zero key", keys.Shared{}},
+		{"full key", keys.Shared{Bytes: []byte{1, 2, 3, 4, 5}}},
 	}
 
 	for _, test := range tests {
-		messageMasterKey, nextHeaderKey, err := chain.Advance(test.sharedKey)
-		if err != nil {
-			t.Fatalf("%+v.Advance(%+v): expected no error but got %v", chain, test.sharedKey, err)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
 
-		if len(messageMasterKey.Bytes) == 0 {
-			t.Fatalf("%+v.Advance(%+v): returned empty message master key %v", chain, test.sharedKey, messageMasterKey)
-		}
+			messageMasterKey, nextHeaderKey, err := chain.Advance(test.sharedKey)
+			if err != nil {
+				t.Fatalf("%+v.Advance(%+v): expected no error but got %v", chain, test.sharedKey, err)
+			}
 
-		if len(nextHeaderKey.Bytes) == 0 {
-			t.Fatalf("%+v.Advance(%+v): returned empty next header key %v", chain, test.sharedKey, nextHeaderKey)
-		}
+			if len(messageMasterKey.Bytes) == 0 {
+				t.Fatalf("%+v.Advance(%+v): returned empty message master key %v", chain, test.sharedKey, messageMasterKey)
+			}
+
+			if len(nextHeaderKey.Bytes) == 0 {
+				t.Fatalf("%+v.Advance(%+v): returned empty next header key %v", chain, test.sharedKey, nextHeaderKey)
+			}
+		})
 	}
 }
 
 func TestChainClone(t *testing.T) {
 	t.Parallel()
 
-	chain, err := New(keys.Root{Bytes: []byte{1, 2, 3, 4, 5}})
-	if err != nil {
-		t.Fatalf("New(): expected no error but got %v", err)
+	tests := []struct {
+		name    string
+		rootKey keys.Root
+	}{
+		{"zero key", keys.Root{}},
+		{"full key", keys.Root{Bytes: []byte{1, 2, 3, 4, 5}}},
 	}
 
-	clone := chain.Clone()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
 
-	if !slices.Equal(clone.rootKey.Bytes, chain.rootKey.Bytes) {
-		t.Fatalf("%+v.Clone(): clone contains different root key: %+v != %+v", chain, clone.rootKey, chain.rootKey)
-	}
+			chain, err := New(test.rootKey)
+			if err != nil {
+				t.Fatalf("New(): expected no error but got %v", err)
+			}
 
-	if &chain.rootKey.Bytes[0] == &clone.rootKey.Bytes[0] {
-		t.Fatalf("%+v.Clone(): clone contains same root key memory pointer %v", chain, clone.rootKey)
+			clone := chain.Clone()
+			if !reflect.DeepEqual(clone.rootKey, chain.rootKey) {
+				t.Fatalf("%+v.Clone(): clone contains different root key: %+v != %+v", chain, clone.rootKey, chain.rootKey)
+			}
+
+			if len(chain.rootKey.Bytes) > 0 && &chain.rootKey.Bytes[0] == &clone.rootKey.Bytes[0] {
+				t.Fatalf("%+v.Clone(): clone contains same root key memory pointer %v", chain, clone.rootKey)
+			}
+		})
 	}
 }
